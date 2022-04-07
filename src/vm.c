8e27f0cca96910e76e840371f0ed4f200a087ae7
Arnau478 <arnauxabia@gmail.com>
2022-04-07 22:45:51 +0200

    Execution tracing debugging

--- src/vm.c
+++ src/vm.c
@@ -1,5 +1,6 @@
 #include "vm.h"
 #include "chunk.h"
+#include "debug.h"
 
 VM vm;
 
@@ -15,7 +16,15 @@ static InterpretResult run(){
 #define READ_CONSTANT() (*vm.ip++)
 #define READ_BYTE() (*vm.ip++)
 
+#ifdef DEBUG
+        printf("== execution trace ==\n");
+#endif
+
     for(;;){
+#ifdef DEBUG
+        disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
+
         uint8_t instruction;
         switch(instruction = READ_BYTE()){
             case OP_CONSTANT:
