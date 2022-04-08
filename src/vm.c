43818d13e563285832b320a15575d48ad0be38d7
Arnau478 <arnauxabia@gmail.com>
2022-04-08 23:39:41 +0200

    Added stack debug print

--- src/vm.c
+++ src/vm.c
@@ -22,6 +22,13 @@ static InterpretResult run(){
 
     for(;;){
 #ifdef DEBUG
+        printf("          ");
+        for(Value *slot = vm.stack; slot < vm.stackTop; slot++){
+            printf("[ ");
+            printValue(*slot);
+            printf(" ]");
+        }
+        printf("\n");
         disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif
 
