0037a7662e347c4aedc6cdf79b8b4e60b64fd9eb
Arnau478 <arnauxabia@gmail.com>
2022-04-10 00:56:08 +0200

    Added true false and nil literals

--- src/value.c
+++ src/value.c
@@ -22,5 +22,15 @@ void writeValueArray(ValueArray *array, Value value){
 }
 
 void printValue(Value value){
-    printf("%g", AS_NUMBER(value));
+    switch(value.type){
+        case VAL_BOOL:
+            printf(AS_BOOL(value) ? "true" : "false");
+            break;
+        case VAL_NIL:
+            printf("nil");
+            break;
+        case VAL_NUMBER:
+            printf("%g", AS_NUMBER(value));
+            break;
+    }
 }
--- src/vm.c
+++ src/vm.c
@@ -67,6 +67,9 @@ static InterpretResult run(){
                 Value constant = READ_CONSTANT();
                 push(constant);
                 break;
+            case OP_NIL: push(NIL_VAL); break;
+            case OP_TRUE: push(BOOL_VAL(true)); break;
+            case OP_FALSE: push(BOOL_VAL(false)); break;
             case OP_ADD:
                 BINARY_OP(NUMBER_VAL, +);
                 break;
