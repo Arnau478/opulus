876ba0f883f27ae49332344b64c5e5ae1f510041
Arnau478 <arnauxabia@gmail.com>
2022-04-10 01:53:33 +0200

    Obj struct inheritance

---
+++ src/object.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "value.h"
+
+#define OBJ_TYPE(value) (AS_OBJ(value)->type)
+
+#define IS_STRING(value) isObjType(value, OBJ_STRING)
+
+#define AS_STRING(value) ((ObjString *)AS_OBJ(value))
+#define AS_CSTRING(value) (((ObjString *)AS_OBJ(value))->chars)
+
+typedef enum {
+    OBJ_STRING,
+} ObjType;
+
+struct Obj {
+    ObjType type;
+};
+
+struct ObjString {
+    Obj obj;
+    int length;
+    char *chars;
+};
+
+static inline bool isObjType(Value value, ObjType type){
+    return IS_OBJ(value) && AS_OBJ(value)->type == type;
+}
--- src/value.h
+++ src/value.h
@@ -5,6 +5,9 @@
 #include <stdbool.h>
 #include "memory.h"
 
+typedef struct Obj Obj;
+typedef struct ObjString ObjString;
+
 typedef enum {
     VAL_BOOL,
     VAL_NIL,
