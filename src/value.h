f80999d563c2762e266096c12782dd712440e502
Arnau478 <arnauxabia@gmail.com>
2022-04-10 00:11:07 +0200

    Created some macros to convert and get value's type

--- src/value.h
+++ src/value.h
@@ -19,6 +19,17 @@ typedef struct {
     } as;
 } Value;
 
+#define IS_BOOL(value) ((value).type == VAL_BOOL)
+#define IS_NIL(value) ((value).type == VAL_NIL)
+#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
+
+#define AS_BOOL(value) ((value).as.boolean)
+#define AS_NUMBER(value) ((value).as.number)
+
+#define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = (value)}})
+#define NIL_VAL(value) ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = (value)}})
+
 typedef struct {
     Value *values;
     int count;
