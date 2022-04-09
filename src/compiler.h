ae801dcc0877ed516f2850f22bb6f76e28ffafe6
Arnau478 <arnauxabia@gmail.com>
2022-04-09 23:40:44 +0200

    Basic compiler capable of parsing basic arithmetic operations

--- src/compiler.h
+++ src/compiler.h
@@ -1,6 +1,37 @@
 #pragma once
 
 #include <stdio.h>
+#include <stdbool.h>
 #include "scanner.h"
+#include "chunk.h"
 
-void compile(const char *source);
+typedef struct {
+    Token current;
+    Token previous;
+    bool hadError;
+    bool panicMode;
+} Parser;
+
+typedef enum {
+    PREC_NONE,
+    PREC_ASSIGNMENT, // =
+    PREC_OR, // or
+    PREC_AND, // and
+    PREC_EQUALITY, // == !=
+    PREC_COMPARISON, // < > <= >=
+    PREC_TERM, // + -
+    PREC_FACTOR, // * /
+    PREC_UNARY, // - !
+    PREC_CALL, // . ()
+    PREC_PRIMARY,
+} Precedence;
+
+typedef void (*ParseFn)();
+
+typedef struct {
+    ParseFn prefix;
+    ParseFn infix;
+    Precedence precedence;
+} ParseRule;
+
+bool compile(const char *source, Chunk *chunk);
