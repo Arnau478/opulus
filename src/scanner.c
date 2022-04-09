8982eb8bdfa1968c67ed88fdb01e9a9ac5aa2c66
Arnau478 <arnauxabia@gmail.com>
2022-04-09 21:05:22 +0200

    Now keywords are converted into their own token type

--- src/scanner.c
+++ src/scanner.c
@@ -111,7 +111,47 @@ static Token number(){
     return makeToken(TOKEN_NUMBER);
 }
 
+static TokenType checkKeyword(int start, int length, const char *rest, TokenType type){
+    if(scanner.current - scanner.start == start + length
+        && memcmp(scanner.start + start, rest, length) == 0){
+        return type;
+    }
+
+    return TOKEN_IDENTIFIER;
+}
+
 static TokenType identifierType(){
+    switch(scanner.start[0]){
+        case 'a': return checkKeyword(1, 2, "nd", TOKEN_AND);
+        case 'c': return checkKeyword(1, 4, "lass", TOKEN_CLASS);
+        case 'e': return checkKeyword(1, 3, "lse", TOKEN_ELSE);
+        case 'f':
+            if(scanner.current - scanner.start > 1){
+                switch(scanner.start[1]){
+                    case 'a': return checkKeyword(2, 3, "lse", TOKEN_FALSE);
+                    case 'o': return checkKeyword(2, 1, "r", TOKEN_FOR);
+                    case 'u': return checkKeyword(2, 1, "n", TOKEN_FUN);
+                }
+            }
+            break;
+        case 'i': return checkKeyword(1, 1, "f", TOKEN_IF);
+        case 'n': return checkKeyword(1, 2, "il", TOKEN_NIL);
+        case 'o': return checkKeyword(1, 1, "r", TOKEN_OR);
+        case 'p': return checkKeyword(1, 4, "rint", TOKEN_PRINT);
+        case 'r': return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
+        case 's': return checkKeyword(1, 4, "uper", TOKEN_SUPER);
+        case 't':
+            if(scanner.current - scanner.start > 1){
+                switch(scanner.start[1]){
+                    case 'h': return checkKeyword(2, 2, "is", TOKEN_THIS);
+                    case 'r': return checkKeyword(2, 2, "ue", TOKEN_TRUE);
+                }
+            }
+            break;
+        case 'v': return checkKeyword(1, 2, "ar", TOKEN_VAR);
+        case 'w': return checkKeyword(1, 4, "hile", TOKEN_WHILE);
+    }
+
     return TOKEN_IDENTIFIER;
 }
 
