8e2d75f42bd0f37e9a335d07e616ae580f3f1d17
Arnau478 <arnauxabia@gmail.com>
2022-04-10 18:36:50 +0200

    Panic mode synchronization

--- src/compiler.c
+++ src/compiler.c
@@ -243,8 +243,33 @@ static void printStatement(){
     emitByte(OP_PRINT);
 }
 
+static void synchronize(){
+    parser.panicMode = false;
+
+    while(parser.current.type != TOKEN_EOF){
+        if(parser.previous.type == TOKEN_SEMICOLON) return;
+        switch(parser.current.type){
+            case TOKEN_CLASS:
+            case TOKEN_FUN:
+            case TOKEN_VAR:
+            case TOKEN_FOR:
+            case TOKEN_IF:
+            case TOKEN_WHILE:
+            case TOKEN_PRINT:
+            case TOKEN_RETURN:
+                return;
+            default:
+                ;
+        }
+
+        advance();
+    }
+}
+
 static void declaration(){
     statement();
+
+    if(parser.panicMode) synchronize();
 }
 
 static void statement(){
