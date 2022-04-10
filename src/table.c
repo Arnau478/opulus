7138c723734b9cda6ee784149b8beb1592c16977
Arnau478 <arnauxabia@gmail.com>
2022-04-10 14:47:04 +0200

    Table allocation

--- src/table.c
+++ src/table.c
@@ -24,6 +24,27 @@ static Entry *findEntry(Entry *entries, int capacity, ObjString *key){
     }
 }
 
+static void adjustCapacity(Table *table, int capacity){
+    Entry *entries = ALLOCATE(Entry, capacity);
+    for(int i = 0; i < capacity; i++){
+        entries[i].key = NULL;
+        entries[i].value = NIL_VAL;
+    }
+
+    for(int i = 0; i < table->capacity; i++){
+        Entry *entry = &table->entries[i];
+        if(entry->key == NULL) continue;
+
+        Entry *dest = findEntry(entries, capacity, entry->key);
+        dest->key = entry->key;
+        dest->value = entry->value;
+    }
+
+    FREE_ARRAY(Entry, table->entries, table->capacity);
+    table->entries = entries;
+    table->capacity = capacity;
+}
+
 bool tableSet(Table *table, ObjString *key, Value value){
     if(table->count + 1 > table->capacity * TABLE_MAX_LOAD){
         int capacity = GROW_CAPACITY(table->capacity);
@@ -37,4 +58,13 @@ bool tableSet(Table *table, ObjString *key, Value value){
     entry->key = key;
     entry->value = value;
     return isNewKey;
+}
+
+void tableAddAll(Table *from, Table *to){
+    for(int i = 0; i < from->capacity; i++){
+        Entry *entry = &from->entries[i];
+        if(entry->key != NULL){
+            tableSet(to, entry->key, entry->value);
+        }
+    }
 }
--- src/table.h
+++ src/table.h
@@ -19,3 +19,4 @@ typedef struct {
 void initTable(Table *table);
 void freeTable(Table *table);
 bool tableSet(Table *table, ObjString *key, Value value);
+void tableAddAll(Table *from, Table *to);
