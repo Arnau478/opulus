163f620f68eed82efaf64fc0f793bfa7abe15fe4
Arnau478 <arnauxabia@gmail.com>
2022-04-14 02:02:26 +0200

    Fixed self-initialization of local variables on nested scopes

--- src/compiler.c
+++ src/compiler.c
@@ -141,6 +141,9 @@ static int resolveLocal(Compiler *compiler, Token *name){
     for(int i = compiler->localCount - 1; i >= 0; i--){
         Local *local = &compiler->locals[i];
         if(identifiersEqual(name, &local->name)){
+            if(local->depth == -1){
+                error("Cannot read variable on its own initialization");
+            }
             return i;
         }
     }
@@ -156,7 +159,7 @@ static void addLocal(Token name){
 
     Local *local = &current->locals[current->localCount++];
     local->name = name;
-    local->depth = current->scopeDepth;
+    local->depth = -1;
 }
 
 static void declareVariable(){
@@ -333,8 +336,13 @@ static uint8_t parseVariable(const char *errorMessage){
     return identifierConstant(&parser.previous);
 }
 
+static void markInitialized(){
+    current->locals[current->localCount - 1].depth = current->scopeDepth;
+}
+
 static void defineVariable(uint8_t global){
     if(current->scopeDepth > 0){
+        markInitialized();
         return;
     }
 
