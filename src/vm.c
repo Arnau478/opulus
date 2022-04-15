b04e39481b63bd061ebc123b82caaa9076c113e2
Arnau478 <arnauxabia@gmail.com>
2022-04-16 01:54:29 +0200

    Stack overflow error

--- src/vm.c
+++ src/vm.c
@@ -30,6 +30,12 @@ static bool call(ObjFunction *function, int argCount){
         runtimeError("Expected %d arguments but got %d", function->arity, argCount);
         return false;
     }
+
+    if(vm.frameCount == FRAMES_MAX){
+        runtimeError("Stack overflow");
+        return false;
+    }
+
     CallFrame *frame = &vm.frames[vm.frameCount++];
     frame->function = function;
     frame->ip = function->chunk.code;
