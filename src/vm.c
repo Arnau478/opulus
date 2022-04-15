39e67e766135ba3f93a752c6548edc70c32ef6f4
Arnau478 <arnauxabia@gmail.com>
2022-04-16 01:52:42 +0200

    Invalid arity runtime error

--- src/vm.c
+++ src/vm.c
@@ -26,6 +26,10 @@ static Value peek(int distance){
 }
 
 static bool call(ObjFunction *function, int argCount){
+    if(argCount != function->arity){
+        runtimeError("Expected %d arguments but got %d", function->arity, argCount);
+        return false;
+    }
     CallFrame *frame = &vm.frames[vm.frameCount++];
     frame->function = function;
     frame->ip = function->chunk.code;
