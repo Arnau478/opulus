59aa46c09b6c036ee0702b372648beda24b9b320
Arnau478 <arnauxabia@gmail.com>
2022-04-15 16:08:15 +0200

    Added missing pop operations to clean stack

--- src/compiler.c
+++ src/compiler.c
@@ -409,11 +409,13 @@ static void ifStatement(){
     consume(TOKEN_RIGHT_PAREN, "Expected ')' after if condition");
 
     int thenJump = emitJump(OP_JUMP_IF_FALSE);
+    emitByte(OP_POP);
     statement();
 
     int elseJump = emitJump(OP_JUMP);
 
     patchJump(thenJump);
+    emitByte(OP_POP);
 
     if(match(TOKEN_ELSE)) statement();
     patchJump(elseJump);
